e691e65d85cf6f9a0fa5d7d83b20ac82d9569620
NotaHuman <dmitriy.busin@gmail.com>
2025-01-26 14:31:52 +0300

    debugged 2nd block (DateTime + Local) + comment for manual debugging 3rd block

--- Dart_tasks/lib/main.dart
+++ Dart_tasks/lib/main.dart
@@ -1,8 +1,65 @@
+// ignore_for_file: unused_import
+
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:Dart_tasks/block2/datetime_utils.dart' as dt;
+import 'package:intl/intl.dart';
 import 'block3/singleton.dart';
 import 'block3/enums.dart';
 
-void main() {
-  // Задача 1: Проверка Singleton для БД
+void main() async {
+  await initializeDateFormatting('ru_RU');
+
+  // Задача 1
+  print('Текущая дата: ${dt.DateTimeUtils.formatCurrentDateTime()}');
+
+  // Задача 2
+  print(dt.DateTimeUtils.compareDates(
+      DateTime(2023, 10, 1), DateTime(2023, 10, 30)));
+
+  // Задача 3
+  print('До Нового года: ${dt.DateTimeUtils.daysToNewYear()} дней');
+
+  // Задача 4
+  print('2024 високосный? ${dt.DateTimeUtils.isLeapYear(2024)}');
+
+  // Задача 5
+  print('Выходных в октябре 2023: ${dt.DateTimeUtils.countWeekends(2023, 10)}');
+
+  // Задача 6
+  dt.DateTimeUtils.measureExecutionTime();
+
+  // Задача 7
+  print(dt.DateTimeUtils.addDaysToDate('31-12-2023'));
+
+  // Задача 8
+  print('Время в Москве: ${dt.DateTimeUtils.convertTimeZone()}');
+
+  // Задача 9
+  print('Возраст: ${dt.DateTimeUtils.calculateAge(DateTime(1990, 5, 15))} лет');
+
+  // Задача 10
+  dt.DateTimeUtils.printMonthCalendar(2023, 10);
+
+  // Задача 11
+  print(
+      'Случайная дата: ${DateFormat('dd.MM.yyyy').format(dt.DateTimeUtils.randomDateInRange(DateTime(2023, 1, 1), DateTime(2023, 12, 31)))}');
+
+  // Задача 12
+  print(dt.DateTimeUtils.timeUntilEvent(DateTime(2023, 12, 31)));
+
+  // Задача 13
+  print(
+      'Рабочих часов: ${dt.DateTimeUtils.calculateWorkingHours(DateTime(2023, 10, 1), DateTime(2023, 10, 31))}');
+
+  // Задача 14
+  print(dt.DateTimeUtils.formatDateWithLocale(DateTime.now(), 'ru_RU'));
+
+  // Задача 15
+  print(dt.DateTimeUtils.getWeekdayName(DateTime.now()));
+}
+
+/* Тут для теста 3-блока
+// Задача 1: Проверка Singleton для БД
   final db1 = DatabaseConnection();
   final db2 = DatabaseConnection();
   print("db1 и db2 один объект? ${identical(db1, db2)}\n"); // true
@@ -30,4 +87,4 @@ void main() {
   }
   // Задача 4: Проверка сезонов
   print(getSeasonName(Season.SUMMER)); // Лето
-}
+*/
