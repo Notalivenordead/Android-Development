ad0b6d93417b58769d24a8b2466f98920b953581
NotaHuman <dmitriy.busin@gmail.com>
2025-02-04 16:10:18 +0300

    optimized R

--- Caesar/lib/core/brute_force.dart
+++ Caesar/lib/core/brute_force.dart
@@ -3,7 +3,6 @@ import '../core/caesar_cipher.dart';
 class BruteForce {
   final CaesarCipher cipher = CaesarCipher();
 
-  List<String> decryptAll(String text, int alphabetSize) {
-    return List.generate(alphabetSize - 1, (i) => cipher.decrypt(text, i + 1));
-  }
+  Future<List<String>> decryptAll(String text, int alphabetSize) async =>
+      List.generate(alphabetSize - 1, (i) => cipher.decrypt(text, i + 1));
 }
